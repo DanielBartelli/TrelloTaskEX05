0689feb5c0bd6acb169ed77d2ae6d104e857a608
Daniel Bartelli <89179404+DanielBartelli@users.noreply.github.com>
2024-02-28 03:03:58 -0300

    add curiosidades

--- scripts/script.js
+++ scripts/script.js
@@ -4,4 +4,10 @@ let dan = document.getElementById("dan");
 
 dan.innerHTML = "@daniel bartelli";
 
-console.log(dan)
+function mudar() {
+    let Curio = document.getElementById("curio")
+    let text = document.getElementById("text")
+
+    Curio.innerHTML = "Curiosidade"; 
+    text.innerHTML = "Tenho o sonho de projetar meus proprios jogos um dia!"
+}
